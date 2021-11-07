fd0df78cb0eabd8bef8d040af27720e6d3f75166
mert özdemir <mertozdemir91@hotmail.com>
2021-11-07 22:25:03 +0300

    Hata mesajlari duzenlendi

--- ArabalarDunyasii/Models/Araba.cs
+++ ArabalarDunyasii/Models/Araba.cs
@@ -9,9 +9,13 @@ namespace ArabalarDunyasii.Models
     public class Araba
     {
         public int Id { get; set; }
-        [Required,MaxLength(200)]
+        [Required(ErrorMessage ="{0} alanı zorunludur. ")]
+        [MaxLength(200, ErrorMessage ="{0} alanı en fazla {1} karakter olabilir.")]
+        [Display(Name ="Model")]
         public string Model { get; set; }
-        [Range(1900,2100)]
-        public int Yil { get; set; }
+        [Required(ErrorMessage ="{0} alanı zorunludur.")]
+        [Range(1900,2100, ErrorMessage ="{0} {1} ile {2} aralığında olmalıdır.")]
+        [Display(Name ="Yıl")]
+        public int? Yil { get; set; }
     }
 }
