e2c79bfcaa9aed6fe53a1210345b80f54006646e
mert özdemir <mertozdemir91@hotmail.com>
2021-11-07 22:16:39 +0300

    arabaEkle

---
+++ ArabalarDunyasii/Controllers/ArabalarController.cs
@@ -0,0 +1,35 @@
+﻿using ArabalarDunyasii.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArabalarDunyasii.Controllers
+{
+    public class ArabalarController : Controller
+    {
+        private readonly UygulamaDbContext db;
+
+        public ArabalarController(UygulamaDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Yeni()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Yeni(Araba araba)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Add(araba);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+    }
+}
